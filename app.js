c9c05f378881dad78a5a01ade31d3e98167d318a
Laudza Farhan <muftylaudzafarhan@gmail.com>
2026-09-19 08:47:41 +0700

    feat(ui): add animated login button with arrow transitions and hover effects

--- app.js
+++ app.js
@@ -1514,9 +1514,15 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     } else {
       userNavContainer.innerHTML = `
-        <button class="btn btn-primary nav-login-btn" id="openAuthModalBtn">
-          <i class="fa-solid fa-user"></i>
-          <span class="btn-text">Login</span>
+        <button class="animated-button nav-login-btn" id="openAuthModalBtn" title="Login">
+          <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+          </svg>
+          <span class="text">LOGIN</span>
+          <span class="circle"></span>
+          <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
+            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+          </svg>
         </button>
       `;
       document.getElementById('openAuthModalBtn').addEventListener('click', () => {
